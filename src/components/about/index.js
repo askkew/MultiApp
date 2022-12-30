24e94c27f90cf15dee7433f03ddf0703a8e1f180
Lucas Overbey <lucasaoverbey@gmail.com>
2022-12-30 16:33:58 -0600

    small changes2

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import VideoBg from '../videoBg'
+import { styled, Box, Card, CardActions, CardContent, Button, Typography } from '@mui/material';
+
+const Root = styled('div')({
+  height: '100vh',
+  width: '100vw',
+  backgroundColor: '#141b2d'
+});
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
 
 export const About = () => {
   return (
-    <div>
-      <VideoBg />
-    </div>
+    <Root>
+        <Card sx={{ maxWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            Lucas Overbey
+          </Typography>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            sample text
+          </Typography>
+          <Typography variant="body2">
+            
+          </Typography>
+          </CardContent>
+        </Card>
+        <VideoBg />
+    </Root>
   )
 }
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { styled } from '@mui/material';
+
+
+const Root = styled('div')({
+  height: '100vh',
+  width: '100vw',
+  backgroundColor: '#141b2d',
+  padding: '10px'
+});
+
+const Root2 = styled('h1')({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center'
+});
 
 export const Home = () => {
   return (
-    <div>Home page</div>
+    <Root>
+      <Root2 style={{color: "#4cceac", }}>
+        Home Page
+      </Root2>
+    </Root>
   )
 }
